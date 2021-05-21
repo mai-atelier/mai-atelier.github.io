5411afd048e329409a800dedf74fee3f7c5a4a3f
Use your GitHub username <Enter your email address>
2021-05-21 11:27:31 +0900

    Initial Commit

--- lesson5/js/preston-5.js
+++ lesson5/js/preston-5.js
@@ -1,23 +1,25 @@
 const now = new Date();
 
-let nortification = document.querySelector('.nortification');
+let inform = document.querySelector('.nortification');
 
 if (now.getDay() == 5){
     let name = document.querySelector('.event-title');
     let info =  document.querySelector('.event-info');
     let button = document.querySelector('.cta');
-    
-    name.innerHTML = '🥞Saturday = Preston Pancakes in the Park!';  
+
+    name.innerHTML = '🥞 Saturday = Preston Pancakes in the Park!';  
     info.innerHTML = '9:00 a.m. Saturday at the city park pavilion.';
     button.innerHTML = 'Check Now';
-
+    
+   
     info.style.display = 'block';
     name.style.display = 'block';
     button.style.display = 'block';
 }
     else{
-        nortification.style.display = 'none';
+        inform.style.display = 'none';
     }
+
 
 
 const drop = document.querySelector('.toggle');
