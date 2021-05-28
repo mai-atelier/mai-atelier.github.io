9116b709572099cca2041abeb59e15d34202a97a
Use your GitHub username <Enter your email address>
2021-05-28 20:47:46 +0900

    Initial Commit

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,11 +1,11 @@
-let temp = 25;
-let speed = 3;
-let chill = document.querySelector('.chill');
+let temp = parseFloat(document.getElementById('current').innerHTML);
+let speed = parseFloat(document.getElementById('wind').innerHTML);
+let wc = document.getElementById('chill');
 
-chill.innerHTML= windchill (temp, speed);
+wc.innerHTML= windchill (temp, speed);
 
 function windchill (temp, speed){
-if (temp<=50 && speed>=3){
+if (temp<=50 && speed>3){
 let factor = 35.74 + 0.6215*temp - 35.75*(speed**0.16) + 0.4275*temp*(speed**0.16); 
 return Math.round(factor);
 }
