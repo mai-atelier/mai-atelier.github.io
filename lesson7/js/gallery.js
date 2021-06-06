f8f22b9d96d2fb83f03223285e2b76e453ae7434
Use your GitHub username <Enter your email address>
2021-06-06 14:06:51 +0900

    Initial Commit

--- lesson7/js/gallery.js
+++ lesson7/js/gallery.js
@@ -1,5 +1,30 @@
 const now = new Date();
 
+let inform = document.querySelector('.nortification');
+
+if (now.getDay() == 5){
+    let pancake = document.querySelector('.pancake');
+    let start = document.querySelector('.start');
+    let middle = document.querySelector('.highlight');
+    let end =  document.querySelector('.end');
+    let info =  document.querySelector('.event-info');
+    let button = document.querySelector('.cta');
+    
+    start.innerHTML = '🥞 Saturday  =';  
+    middle.innerHTML = ' Preston Pancakes ';
+    end.innerHTML ='in the Park!'; 
+   
+    info.innerHTML = '9:00 a.m. Saturday at the city park pavilion.';
+    button.innerHTML = 'Check Now';
+
+    pancake.style.display = 'block';
+    info.style.display = 'block';
+    button.style.display = 'block';
+}
+    else{
+        inform.style.display = 'none';
+    }
+
 const hambutton = document.querySelector('.drop');
 const menu = document.querySelector('.hide');
 
@@ -10,12 +35,12 @@ document.getElementById('date').innerHTML = new Intl.DateTimeFormat("en-US",{ da
 
 
 function dayOfNumbers(){
-
 const lastVisit = localStorage.getItem('date');
 let convert = 1000*60*60*24;
 let seconds = now.getTime() -lastVisit;
 let days = Math.round(seconds/convert);
 return days;
 }
+
 document.getElementById('user').innerHTML = dayOfNumbers();
 const update = localStorage.setItem('date',now.getTime());
