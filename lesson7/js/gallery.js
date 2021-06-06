024cc934bded44dff7049eed1de653432fc4f2b8
Use your GitHub username <Enter your email address>
2021-06-06 13:54:01 +0900

    Initial Commit

--- lesson7/js/gallery.js
+++ lesson7/js/gallery.js
@@ -7,3 +7,13 @@ hambutton.addEventListener('click', () => {menu.classList.toggle('responsive')},
 
 document.getElementById('year').innerHTML = now.getFullYear();
 document.getElementById('date').innerHTML = new Intl.DateTimeFormat("en-US",{ dateStyle:"full"}).format(now);
+
+function dayOfNumbers(){
+const lastViist = localStorage.getItem('date');
+let convert = 1000*60*60*24;
+let seconds = now.getTime() -lastVisit;
+let days = Math.round(days/convert);
+return days;
+}
+document.getElementById('user').innerHTML = dayOfNumbers();
+const update = localStorage.setItem('date',now.getTime());
