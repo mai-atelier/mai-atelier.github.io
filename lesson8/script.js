14250142c1d981d2db49eea81095116e70c1cea5
Use your GitHub username <Enter your email address>
2021-06-12 18:53:21 +0900

    Initial Commit

---
+++ lesson8/script.js
@@ -0,0 +1,45 @@
+const now = new Date();
+
+let inform = document.querySelector('.notification');
+
+if (now.getDay() == 5){
+    let pancake = document.querySelector('.pancake');
+    let start = document.querySelector('.start');
+    let middle = document.querySelector('.highlight');
+    let end =  document.querySelector('.end');
+    let info =  document.querySelector('.event-info');
+    let button = document.querySelector('.cta');
+    
+    start.innerHTML = '🥞 Saturday  =';  
+    middle.innerHTML = ' Preston Pancakes ';
+    end.innerHTML ='in the Park!'; 
+   
+    info.innerHTML = '9:00 a.m. Saturday at the city park pavilion.';
+    button.innerHTML = 'Check Now';
+
+    pancake.style.display = 'block';
+    info.style.display = 'block';
+    button.style.display = 'block';
+}
+    else{
+        inform.style.display = 'none';
+    }
+
+const hambutton = document.querySelector('.drop');
+const menu = document.querySelector('.hide');
+
+hambutton.addEventListener('click', () => {menu.classList.toggle('responsive')}, false);
+
+document.getElementById('year').innerHTML = now.getFullYear();
+document.getElementById('date').innerHTML = new Intl.DateTimeFormat("en-US",{ dateStyle:"full"}).format(now);
+
+
+function dayOfNumbers(){
+const lastVisit = localStorage.getItem('date');
+let convert = 1000*60*60*24;
+let seconds = now.getTime() -lastVisit;
+let days = Math.round(seconds/convert);
+return days;
+}
+document.getElementById('user').innerHTML = dayOfNumbers() ;
+const update = localStorage.setItem('date',now.getTime());
