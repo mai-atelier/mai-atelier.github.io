7edd01cb2fdff5d9511c0ac048a99bbefa628cc1
Use your GitHub username <Enter your email address>
2021-06-18 00:12:04 +0900

    Initial Commit

--- lesson9/js/script.js
+++ lesson9/js/script.js
@@ -97,6 +97,24 @@ fetch (requestURL)
 document.getElementById('year').innerHTML = now.getFullYear();
 document.getElementById('date').innerHTML = new Intl.DateTimeFormat("en-US",{ dateStyle:"full"}).format(now);
 
+/* windchill calculation*/
+let temp = parseFloat(document.getElementById('current').innerHTML);
+let speed = parseFloat(document.getElementById('wind').innerHTML);
+let wc = document.getElementById('chill');
+
+wc.innerHTML= windchill (temp, speed);
+
+function windchill (temp, speed){
+if (temp<=50 && speed>3){
+let factor = 35.74 + 0.6215*temp - 35.75*(speed**0.16) + 0.4275*temp*(speed**0.16); 
+return Math.round(factor);
+}
+else {
+return factor = 'N/A';
+}
+
+}
+   
 /* gallery page */
 /* lazy loading */
 const images = document.querySelectorAll('img[data-src]');
