48b5fc0d1d8f12b6feab60053eca1adc980c718e
Use your GitHub username <Enter your email address>
2021-06-28 16:03:09 +0900

    Initial Commit

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,15 @@
+const cityid = "5604473";
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=8f806e2b6239d7e309c5ca0798079e25&units=imperial"
+
+fetch(apiURL)
+.then((response)=>response.json())
+.then((jsObject)=>{ console.log(jsObject);
+    document.getElementById('current').textContent = jsObject.main.temp;
+    document.getElementById('condition').textContent = jsObject.main.temp;
+
+const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+const desc = jsObject.weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
+});
