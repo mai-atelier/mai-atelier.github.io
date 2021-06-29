a112c8026d7b08efa5be948867bd8992e06a3693
Use your GitHub username <Enter your email address>
2021-06-29 17:51:56 +0900

    Initial Commit

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -15,7 +15,7 @@ fetch(apiURL)
 const forecastURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=8f806e2b6239d7e309c5ca0798079e25&units=imperial"
 fetch(forecastURL)
 .then((response)=>response.json())
-.then((jsObject)=>{ console.log(jsObject);
+.then((jsObject)=>{ //console.log(jsObject);
 
    
     const fivedaysForecast = jsObject.list.filter( forecast => forecast.dt_txt.includes('18:00:00'));
@@ -34,19 +34,19 @@ fetch(forecastURL)
     const days = document.createElement('p');
         days.setAttribute = ('id','dayname');
         days.textContent = displayDay;
-        console.log(displayDay);
+        //console.log(displayDay);
 
-    const temp = document.createElement('p');
-        temp.className = 'hightemp';
-        temp.textContent = `${element.main.temp.toFixed(0)}°`;  
+    const ft = document.createElement('p');
+        ft.className = 'hightemp';
+        ft.textContent = `${element.main.temp.toFixed(0)}°`;  
          
-        console.log(temp);
+        //console.log(temp);
         const imagesrc = 'https://openweathermap.org/img/w/'+element.weather[0].icon + '.png'; 
         const desc = element.weather[0].description; 
         let icon = document.createElement('img');
         icon.setAttribute('src', imagesrc);  // focus on the setAttribute() method
         icon.setAttribute('alt', desc);
-        div.append(days, icon, temp);
+        div.append(days, icon, ft);
         document.querySelector('div.dailyweather').appendChild(div); 
 
         i++
